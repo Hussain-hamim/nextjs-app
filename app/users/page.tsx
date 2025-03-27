f917f24b0d4cfc236e8ccac809f8a0b77ff791e3
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-27 19:01:04 +0430

    static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,16 +6,23 @@ interface User {
   email: string;
 }
 
+// run in production: npm start
+// run in development: npm run dev
+
+// Rendering: 1) client-side: browser, 2) server-side: server => faster load time => two types of rendering
+// server-side: 1) static (at build time): 2) dynamic (at request time)
+
 const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    // cache: 'no-store',
-    next: { revalidate: 10 },
+    cache: 'no-store', // we make the data dynamic by not caching it, we want to fetch the data every time
+    // next: { revalidate: 10 }, // by default fetch is static data or unchanging data
   });
   const users: User[] = await res.json(); // we do not need state management here because this is rending on the server side
 
   return (
     <div>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
@@ -26,5 +33,4 @@ const UsersPage = async () => {
     </div>
   );
 };
-
 export default UsersPage;
