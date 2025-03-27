a948b769ce51104a36f9a10834fa759ef7a8a39a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-27 15:35:54 +0430

    client and server components; use client

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <div>ProductCard</div>
+      <button onClick={() => console.log('click')}>add to cart</button>
+    </div>
+  );
+};
+
+export default ProductCard;
