a2a3ef5c05dde15a0e9853b63ec45e16086dba86
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-28 15:59:20 +0430

    programmatic navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import ProductCard from './components/ProductCard';
 
 export default function Home() {
--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,17 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const NewUserPage = () => {
-  return <div>NewUserPage </div>;
+  const router = useRouter();
+
+  // we cannot do browser events in server right!
+
+  return (
+    <button onClick={() => router.push('/users')} className='btn btn-primary '>
+      Create
+    </button>
+  );
 };
 
 export default NewUserPage;
