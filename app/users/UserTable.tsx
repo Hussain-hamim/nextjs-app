6992cfebb207fd0b0329bc5da493e3e02824a68d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-28 14:43:09 +0430

    daisy ui table

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -15,14 +15,22 @@ const UserTable = async () => {
 
   return (
     <>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} - {user.email}
-          </li>
-        ))}
-        {/* <h1>hello</h1> */}
-      </ul>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
