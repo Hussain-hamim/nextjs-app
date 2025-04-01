5f154bbe8b07f36a410f328638a1001bdb84deb2
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-01 15:48:49 +0430

    products api

---
+++ app/api/products/route.tsx
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+import schema from './schema';
+
+export function GET(request: NextRequest) {
+  return NextResponse.json([
+    { id: 1, name: 'Phone', price: '2.5' },
+    { id: 2, name: 'PC', price: '5.1' },
+  ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const validation = schema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  return NextResponse.json(
+    { id: 1, name: body.name, price: body.price },
+    { status: 201 }
+  );
+}
