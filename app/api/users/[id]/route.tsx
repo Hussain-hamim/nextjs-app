2c4f508d6092f002f0b5fed26efa380558690c4c
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-01 23:36:33 +0430

    update user

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -26,10 +26,22 @@ export async function PUT(request: NextRequest, { params }: Props) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 404 });
 
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: Number(params.id) },
+  });
+
+  if (!user)
     return NextResponse.json({ error: 'user not found' }, { status: 404 });
 
-  return NextResponse.json({ id: params.id, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(request: NextRequest, { params }: Props) {
