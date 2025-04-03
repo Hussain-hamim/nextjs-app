b161d389a342195b8a2058605c4eaa3509c7ce4f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-03 11:46:28 +0430

    google auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className='flex bg-slate-200 text-gray-600 p-5 mb-2'>
+    <div className='flex bg-slate-200 text-gray-600 p-5 mb-2 space-x-3'>
       <Link className='mr-5' href='/'>
         Next.js
       </Link>
@@ -11,6 +11,7 @@ const NavBar = () => {
         Users
       </Link>
       <Link href='/admin'>Admin</Link>
+      <Link href='/api/auth/signin'>Login</Link>
     </div>
   );
 };
