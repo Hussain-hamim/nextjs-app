9b72acff4de07bab58a43d5cf6fad0d201c17d14
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-03 11:57:42 +0430

    working on session

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,13 @@
+'use client';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
+  if (status === 'loading') return null;
+
   return (
     <div className='flex bg-slate-200 text-gray-600 p-5 mb-2 space-x-3'>
       <Link className='mr-5' href='/'>
@@ -11,7 +17,10 @@ const NavBar = () => {
         Users
       </Link>
       <Link href='/admin'>Admin</Link>
-      <Link href='/api/auth/signin'>Login</Link>
+      {status === 'authenticated' && <div>{session.user?.name}</div>}
+      {status === 'unauthenticated' && (
+        <Link href='/api/auth/signin'>Login</Link>
+      )}
     </div>
   );
 };
