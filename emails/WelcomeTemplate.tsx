2ac0982c32e934b9fddad0a7cf0cbbb56c76e394
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-04 18:03:58 +0430

    email style

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,14 +1,23 @@
-import { Body, Html, Link, Preview, Text } from '@react-email/components';
+import {
+  Body,
+  Html,
+  Link,
+  Preview,
+  Tailwind,
+  Text,
+} from '@react-email/components';
 import React from 'react';
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome here!</Preview>
-      <Body>
-        <Text>Hello {name}</Text>
-        <Link href='x.com/erencodes'>follow on our X</Link>
-      </Body>
+      <Tailwind>
+        <Body className='bg-blue-300'>
+          <Text>Hello {name}</Text>
+          <Link href='x.com/erencodes'>follow on our X</Link>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
